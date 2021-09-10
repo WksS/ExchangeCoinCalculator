85b5d5cc0c26a4711ae5cf2f92e759d5dd8540cc
Nathan Burgos <naythan035@gmail.com>
2021-09-10 17:39:37 -0300

    Improved design

--- ExchangeCalculator/ExchangeCalculator.Designer.cs
+++ ExchangeCalculator/ExchangeCalculator.Designer.cs
@@ -38,7 +38,7 @@ namespace ExchangeCalculator
             this.textOfFIrstValue = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.comboBoxType = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.SuspendLayout();
@@ -46,6 +46,7 @@ namespace ExchangeCalculator
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(51)))), ((int)(((byte)(63)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
@@ -56,23 +57,23 @@ namespace ExchangeCalculator
             // 
             // panel2
             // 
-            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(35)))), ((int)(((byte)(35)))));
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
             this.panel2.Controls.Add(this.label1);
             this.panel2.Location = new System.Drawing.Point(0, 0);
             this.panel2.Margin = new System.Windows.Forms.Padding(5);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(260, 134);
+            this.panel2.Size = new System.Drawing.Size(259, 128);
             this.panel2.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
-            this.label1.Font = new System.Drawing.Font("Century Gothic", 22F, System.Drawing.FontStyle.Bold);
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 20F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.SystemColors.Control;
-            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Location = new System.Drawing.Point(23, 9);
             this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(260, 134);
+            this.label1.Size = new System.Drawing.Size(210, 110);
             this.label1.TabIndex = 0;
             this.label1.Text = "Exchange Rate Calculator";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -80,28 +81,34 @@ namespace ExchangeCalculator
             // valueToConvert
             // 
             this.valueToConvert.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(54)))), ((int)(((byte)(62)))));
-            this.valueToConvert.Location = new System.Drawing.Point(452, 65);
+            this.valueToConvert.Font = new System.Drawing.Font("Century Gothic", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.valueToConvert.ForeColor = System.Drawing.SystemColors.Control;
+            this.valueToConvert.Location = new System.Drawing.Point(473, 59);
             this.valueToConvert.Multiline = true;
             this.valueToConvert.Name = "valueToConvert";
-            this.valueToConvert.Size = new System.Drawing.Size(134, 69);
+            this.valueToConvert.Size = new System.Drawing.Size(134, 51);
             this.valueToConvert.TabIndex = 1;
             this.valueToConvert.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // button1
             // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(42)))), ((int)(((byte)(64)))));
-            this.button1.Location = new System.Drawing.Point(420, 319);
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.Control;
+            this.button1.Location = new System.Drawing.Point(436, 336);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(207, 95);
+            this.button1.Size = new System.Drawing.Size(197, 69);
             this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
+            this.button1.Text = "Calculate";
             this.button1.UseVisualStyleBackColor = false;
             // 
             // textOfFIrstValue
             // 
             this.textOfFIrstValue.AutoSize = true;
             this.textOfFIrstValue.Font = new System.Drawing.Font("Century Gothic", 18F);
-            this.textOfFIrstValue.Location = new System.Drawing.Point(380, 21);
+            this.textOfFIrstValue.ForeColor = System.Drawing.SystemColors.Control;
+            this.textOfFIrstValue.Location = new System.Drawing.Point(390, 9);
             this.textOfFIrstValue.Name = "textOfFIrstValue";
             this.textOfFIrstValue.Size = new System.Drawing.Size(301, 30);
             this.textOfFIrstValue.TabIndex = 4;
@@ -110,29 +117,40 @@ namespace ExchangeCalculator
             // textBox1
             // 
             this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(54)))), ((int)(((byte)(62)))));
-            this.textBox1.Location = new System.Drawing.Point(268, 452);
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(315, 428);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(584, 27);
+            this.textBox1.Size = new System.Drawing.Size(480, 51);
             this.textBox1.TabIndex = 5;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 18F);
-            this.label2.Location = new System.Drawing.Point(499, 155);
+            this.label2.ForeColor = System.Drawing.SystemColors.Control;
+            this.label2.Location = new System.Drawing.Point(521, 155);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(47, 30);
             this.label2.TabIndex = 6;
             this.label2.Text = "To:";
             // 
-            // listBox1
+            // comboBoxType
             // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.ItemHeight = 21;
-            this.listBox1.Location = new System.Drawing.Point(452, 219);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(150, 25);
-            this.listBox1.TabIndex = 7;
+            this.comboBoxType.AutoCompleteCustomSource.AddRange(new string[] {
+            "Dolar",
+            "Euro"});
+            this.comboBoxType.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(52)))), ((int)(((byte)(54)))), ((int)(((byte)(62)))));
+            this.comboBoxType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxType.ForeColor = System.Drawing.SystemColors.Control;
+            this.comboBoxType.FormattingEnabled = true;
+            this.comboBoxType.Items.AddRange(new object[] {
+            "Dolar",
+            "Euro"});
+            this.comboBoxType.Location = new System.Drawing.Point(473, 223);
+            this.comboBoxType.Name = "comboBoxType";
+            this.comboBoxType.Size = new System.Drawing.Size(134, 29);
+            this.comboBoxType.TabIndex = 4;
             // 
             // ExchangeCalculator
             // 
@@ -140,7 +158,7 @@ namespace ExchangeCalculator
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
             this.ClientSize = new System.Drawing.Size(864, 491);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBoxType);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.textOfFIrstValue);
@@ -169,7 +187,7 @@ namespace ExchangeCalculator
         private System.Windows.Forms.Label textOfFIrstValue;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox comboBoxType;
     }
 }
 
